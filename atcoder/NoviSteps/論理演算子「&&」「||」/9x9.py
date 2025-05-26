64ff3cc61e6fcc5cc8d442502be32d0bb6dd022e
Yeahjun Heo <yeahjunheo@gmail.com>
2025-05-27 01:02:19 +0900

    done for the day

---
+++ atcoder/NoviSteps/論理演算子「&&」「||」/9x9.py
@@ -0,0 +1,5 @@
+A,B = map(int, input().split())
+if A >= 10 or B >= 10:
+    print(-1)
+else:
+    print(A * B)
---
+++ atcoder/NoviSteps/論理演算子「&&」「||」/welcomeToAtcoderLand.py
@@ -0,0 +1,5 @@
+s,t = map(str,input().split())
+if s == 'AtCoder' and t == 'Land':
+    print("Yes")
+else:
+    print("No")
